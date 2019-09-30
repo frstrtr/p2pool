c3dc88fcae616ef68742aded387887fd5f6174cd
frstrtr <frstrtr@gmail.com>
2019-09-30 21:58:22 +0300

    Human friendly date time format

--- data_utils/share_parser.py
+++ data_utils/share_parser.py
@@ -1,6 +1,12 @@
 # http://p2pool_node_address:port/web/share/FULL_SHARE_HASH
 import json
-x = '''{"parent": "01b37570d7d051cbabbfe5346e2616a01b2d30fb3891482d57d584682a893544", "far_parent": "7c4d2a8cc178da712051adbae96d64bfdf41e3ab46fa704936e4a88d8838f636", "children": ["0e0aded8ae4118f918aa64e0601a793338ea9c23b7b4119c201b26a12187ee50"], "type_name": "SegwitMiningShare", "local": {"verified": true, "time_first_seen": 1569855014.741917, "peer_first_received_from": ["104.42.74.172", 42926]}, "share_data": {"timestamp": 1569855009, "target": 204855675758224598306640411455836090894735482053417996229890015232, "max_target": 4405512243804544602843994967992560721082963273026172177170334482432, "payout_address": "LZzzckEZYhCySGSt7R7RtnpJPLnU1iN8q8", "donation": 0.0, "stale_info": null, "nonce": 3877144208, "desired_version": 34, "absheight": 12816371, "abswork": 29467199543963842321}, "block": {"hash": "dc9aceecac61bc89961294e600391127755159d8a06948f7f1075a1ad2537061", "header": {"version": 536870912, "previous_block": "6c234ce57c79efea6111bce210cb6dd0b10bd3c313a5457936192781328050c5", "merkle_root": "7860d9eecc2c6cdba891dd1e3dc2ffd8a6b48feeb43cb422f09c44353b9721f5", "timestamp": 1569854995, "target": 2486002006428317978918043680667014355192849654093261981089792, "nonce": 599714903}, "gentx": {"hash": "dcced93f40a773dbfbd0cf8e809b4fcd139a0dce4d810c3c27c88c6f826d6c8c", "coinbase": "036c1d1a2cfabe6d6d50026315ddce7f0135c4284fbf4d1c5adeaa4b1d287ef65d2b11cd0af9e5b93c40000000000000002c536962657269616e207365727665723a20687474703a2f2f736962657269612e6d696e652e6e753a39333237", "value": 12.50408574, "last_txout_nonce": "0000000300000000"}, "other_transaction_hashes": []}}'''
+from datetime import datetime
+# ts = int("1284101485")
+
+# if you encounter a "year is out of range" error the timestamp
+# may be in milliseconds, try `ts /= 1000` in that case
+# print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+x = '''{"parent": "54e5bc1b56158efc9acd69bf43ac51238affb07a1b06e21a07a3083766793b80", "far_parent": "7e4c77963a07ce6b4eb0ee680d93aef30db0ec4bcd7d37f48c0a1c93a9e09a9e", "children": ["82fcc2a71b6fd5ce31d01d6472ba3a165939f23a763d84d091a0a36572a93a05"], "type_name": "SegwitMiningShare", "local": {"verified": true, "time_first_seen": 1569869196.022098, "peer_first_received_from": ["95.79.35.133", 9326]}, "share_data": {"timestamp": 1569869193, "target": 156927141650155954348325850089668575111908255560852417124191174656, "max_target": 4707815856442722889440051044652149594519850189028566296518570541056, "payout_address": "LRW6WhZbr4uWCSYD5Eg2Roztnzhp9K6w2h", "donation": 0.0, "stale_info": null, "nonce": 1385289580, "desired_version": 34, "absheight": 12817311, "abswork": 29467678264036718636}, "block": {"hash": "ca5fca0931f3740a6cb22926a6ea0f67e9699cf1f55ce7dbffcdc46cc0f1acc0", "header": {"version": 536870912, "previous_block": "db7291fe459eff090c5926c289052e30ad23035fdc3f89de71c9d452af4517b9", "merkle_root": "248e2f04c34817abe3799aed4a651f9c94c7e83441ae462ccc62a8138bb9b15c", "timestamp": 1569869190, "target": 2486002006428317978918043680667014355192849654093261981089792, "nonce": 2103207830}, "gentx": {"hash": "22f295abf113dc1c80586827594ea8ded9b88c2f5841946b1a352b380a18a6d8", "coinbase": "03be1d1a", "value": 12.51564297, "last_txout_nonce": "0000000100000004"}, "other_transaction_hashes": []}}'''
 # share data example output
 
 
@@ -13,10 +19,15 @@ def share_parser(share_json_string):
             print 'SHARE DATA:', '\n'
             for n1 in share_json[n]:
                 print '\t', n1, ':', share_json[n][n1], '\n'
+                if n1 == 'timestamp':
+                    print '\t', n1, 'human friendly:', datetime.utcfromtimestamp(share_json[n][n1]).strftime('%Y-%m-%d %H:%M:%S'),'\n'
         elif n == 'local':
             print 'LOCAL:', '\n'
             for n2 in share_json[n]:
                 print '\t', n2, ':', share_json[n][n2], '\n'
+                if n2 == 'time_first_seen':
+                    print '\t', n2, 'human friendly:', datetime.utcfromtimestamp(share_json[n][n2]).strftime('%Y-%m-%d %H:%M:%S'),'\n'
+                
         elif n == 'block':
             print 'BLOCK:', '\n'
             for n3 in share_json[n]:
@@ -24,13 +35,14 @@ def share_parser(share_json_string):
                     print '\t', 'HEADER:', '\n'
                     for n4 in share_json[n][n3]:
                         print '\t\t', n4, ':', share_json[n][n3][n4], '\n'
+                        if n4 == 'timestamp':
+                            print '\t\t', n4, 'human friendly:', datetime.utcfromtimestamp(share_json[n][n3][n4]).strftime('%Y-%m-%d %H:%M:%S'),'\n'
                 elif n3 == 'gentx':
                     print '\t', 'GENTX:', '\n'
                     for n4 in share_json[n][n3]:
                         if n4 == 'coinbase':
                             # Add esc symbols for string output (better look)
-                            repr_string = repr(
-                                share_json[n][n3][n4].decode("hex"))
+                            repr_string = repr(share_json[n][n3][n4].decode("hex"))
                             print '\t\t', n4, ':', repr_string, '\n'
                         else:
                             print '\t\t', n4, ':', share_json[n][n3][n4], '\n'
