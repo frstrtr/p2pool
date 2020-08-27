71d49cf9c96b378d5ff2679f71fc032bb80ff518
frstrtr <frstrtr@gmail.com>
2020-08-27 06:48:54 +0300

    ismine deprecation fix

--- p2pool/main.py
+++ p2pool/main.py
@@ -137,6 +137,12 @@ def main(args, net, datadir_path, merged_urls, worker_endpoint):
                 try:
                     res = yield deferral.retry('Error validating cached address:', 5, 1)(lambda: bitcoind.rpc_getaddressinfo(address))()# rpc_validateaddress(address))()
                     if not res['ismine']:
+                        print '    Cached address is invalid!'
+                        address = None
+                    else:
+                    # checks address controlled by local bitcoind
+                    res = yield deferral.retry('Error validating cached address:', 5)(lambda: bitcoind.rpc_getaddressinfo(address))()
+                    if not res['ismine']:
                         print '    Cached address is not controlled by local bitcoind!'
                         address = None
                     # validateaddress DEPRECATION WARNING: Parts of this command have been deprecated and moved to getaddressinfo.
